b5adb4e794089b3210764061b41d2e2e6c9916fe
Andrea Tonin <andreat@lucedigitale.com>
2018-05-25 16:11:47 +0200

    Primo Commit con NetBeans

--- home.php
+++ home.php
@@ -3,8 +3,8 @@
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. bla bla bla 6
  */
 
-echo 'This is home modified - 4';
+echo 'This is home modified - 5';
 
--- index.php
+++ index.php
@@ -6,4 +6,4 @@
  * and open the template in the editor.
  */
 
-echo 'This is index - change 3';
+echo 'This is index - change 5';
